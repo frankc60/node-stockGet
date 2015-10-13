a02052359c05e88e5ed957003012b539dd9451c0
frankc60 <frankc60@gmail.com>
2015-10-13 11:14:30 +0100

    updated everything, tidy up

--- finQuery.js
+++ finQuery.js
@@ -19,8 +19,8 @@
 var cheerio = require('cheerio');
 var https = require('https');
 
-var debugMode=1;
-var baseurl="https://www.goddogle.co.uk/finance?q=";
+var debugMode=0;
+var baseurl="https://www.google.co.uk/finance?q=";
 
 //var query = "NASDAQ%3AAAPL";
 
@@ -36,34 +36,57 @@ function dlog(data){
 
 var Fin = function() {};
 
+//***********************************************************************
 Fin.prototype.url = function (query,callback) {
 	dlog('url to query is '+ baseurl + query);
 	callback(baseurl+query);
 	return;
 };
+//***********************************************************************
 
 
-
+//***********************************************************************
 Fin.prototype.query = function(symbol,attri,callback) {
-	//start timing
-	//console.time("webrequest");
 
 	dlog("\n-----\nsymbol:"+symbol+"\nattri:"+attri+"\n-----\n");
 
-	var req = https.get((baseurl+symbol), function(res){
+	var req = https.request((baseurl+symbol), function(res){
 		
 		var body = "";
 
+		dlog(res.sockets);
+	//***********************************************
+		res.setTimeout(4500, function(e){
+			console.log("timed out");
+			callback("error");
+			return;
+		});
+	//***********************************************
 		res.on('data', function (htmldata) {
 			body += htmldata;
+			dlog("data..");
+
 		});
+	//***********************************************
+		res.on('error', function() {	
+			console.log("sdfasfaf");
+			req.abort();
+			req.destroy();
 
+			req.end();
+			callback(new Error(e),null);
 
-
-
-
+		});
+	//***********************************************
+//		res.on("socket", function (socket) {
+//		  socket.emit("agentRemove");
+//		  socket.emit("end");
+		  
+//		});
+	//***********************************************
 	  	res.on('end', function() {
 			//need statusCode of 200 otherwise we have an issue
+			 //clearTimeout( timeout );
 			if(res.statusCode !=200)
 			{
 				dlog("error statusCode: " + res.statusCode);
@@ -71,115 +94,118 @@ Fin.prototype.query = function(symbol,attri,callback) {
 				return;
 			}
 
+
 	    	dlog('No more data in response.');
 	  		
 	  		
-	  		//end timing
-	  		//	console.timeEnd("webrequest");
-
 	  		//manipulate DOM
+			manDom(body);
+		});
+	});
+
+function manDom(htmlbody)
+{
+	var $ = cheerio.load(htmlbody);
+	var stockSymbol= {};
+	if(attri=="all"){
+		var k="";
+		$(htmlbody).find('meta[itemprop]').each (function(){
+		 	attrib = ($(this).attr('itemprop'));
+		 	valuu = ($(this).attr('content'));
+		 	stockSymbol[attrib] = valuu;
+		 });
+		callback(null,stockSymbol);
+		return;
+	}
+	else
+	{
+
+		if($("meta[itemprop="+attri+"]").attr("content") === undefined)
+		{
+			console.log("Error: missing parameters, attribute is " +attri+".");
+			dlog("error ´"+attri+"´ not found - callbacking with error");
+			if(callback){
+				callback(new Error("´"+attri + "´ not found."),null);
+			}
+			else
 			{
-			var $ = cheerio.load(body);
+				return;
+			}
+			
+		}
+		else
+		{
 			var stockSymbol= {};
-			if(attri=="all"){
-				var k="";
-				$(body).find('meta[itemprop]').each (function(){
-
-				 	attrib = ($(this).attr('itemprop'));
-				 	valuu = ($(this).attr('content'));
-				 	//valuu = $("meta[itemprop="+attrib+"]").attr("content");
-				 	//k += "\"" +attrib + "\":\""+valuu+"\",";
-				 	stockSymbol[attrib] = valuu;
-
-
-				 });
-   				//k = k.substring(0, k.length - 1);
-   				//k = "{" + k + "}";
- 				//stockSymbol = JSON.parse(k);
-
-/*
-				var name 				= $("meta[itemprop=name]").attr("content");
-				var tickerSymbol		= $("meta[itemprop=tickerSymbol]").attr("content");
-				var exchange 			= $("meta[itemprop=exchange]").attr("content");
-				var exchangeTimezone	= $("meta[itemprop=exchangeTimezone]").attr("content");
-				var price 				= $("meta[itemprop=price]").attr("content");
-				var priceChange 		= $("meta[itemprop=priceChange]").attr("content");
-				var priceChangePercent	= $("meta[itemprop=priceChangePercent]").attr("content");
-				var quoteTime			= $("meta[itemprop=quoteTime]").attr("content");
-
-				var stockSymbol = { 
-				    "name": name, 
-				    "tickerSymbol": tickerSymbol,
-				    "exchange": exchange,
-				    "exchangeTimezone": exchangeTimezone,
-				    "price":price,
-				    "priceChange": priceChange,
-				    "priceChangePercent": priceChangePercent,
-				    "quoteTime":quoteTime
-				}
-*/
-				callback(null,stockSymbol);
+			stockSymbol[attri] = $("meta[itemprop="+attri+"]").attr("content");
+			
+			if(stockSymbol[attri] == ""){
+				dlog("error - "+attri+" doesn´t seem to exist!");
+				callback(new Error(attri + " doesn´t exist"),null);
 				return;
 			}
 			else
 			{
-				if($("meta[itemprop="+attri+"]").attr("content") === undefined)
-				{
-					dlog("error "+attri+" not found - callbacking with error");
-					callback(new Error("´"+attri + "´ not found."),null);
-					return;
-				}
-				else
-				{
-					var stockSymbol= {};
-					stockSymbol[attri] = $("meta[itemprop="+attri+"]").attr("content");
-					
-					if(stockSymbol[attri] == ""){
-						dlog("error - "+attri+" doesn´t seem to exist!");
-						callback(new Error(attri + " doesn´t exist"),null);
-						return;
-					}
-					else
-					{
-
-						//dlog("\nx:"+attri+":\t\t\t\"" + $("meta[itemprop="+attri+"]").attr("content") + "\"");
-						//dlog("\nZZZZZZ:"+attri+":\t\t\t" + stockSymbol[attri]);
-					}
-				}
-				
-				var string = JSON.stringify(stockSymbol);
-				dlog("\n\nString:"+string);
-
-				dlog("\n\n");
-
+				//success - pass back data to callback.
 				callback(null,stockSymbol);
 				return;
-				}
 			}
-		});
+		}
+		
+	//	var string = JSON.stringify(stockSymbol);
+	//	dlog("\n\nString:"+string);
+
+	//	dlog("\n\n");
+
+	//	callback(null,stockSymbol);
+	//	return;
+	}
+		
+} //end function
+//*****************************************************
+
+	req.on('error', function(e) {
+	  //console.error(e);
+	  req.abort();
+	  req.destroy();
+
+	  req.end();
+	  callback(new Error(e),null);
+	  
 	});
 
-		req.on('error', function(e) {
-		  console.error(e);
-		  callback(e);
-		  return;
-		});
+
+	req.on('timeout', function () {
+	  // Timeout happend. Server received request, but not handled it
+	  // (i.e. doesn't send any response or it took to long).
+	  // You don't know what happend.
+	  // It will emit 'error' message as well (with ECONNRESET code).
+	  
+	  console.log('timeout');
+	  req.abort();
+	  req.destroy();
+	  req.end();
+	  //req.finish();
+	});
+
+	req.on("socket", function (socket) {
+	  dlog("socket start");
+	//  socket.emit("agentRemove");
+	 // socket.emit("close");
+
+	  
+	});
 
 
-req.on('socket', function (socket) {
-    socket.setTimeout(3000);  
-    socket.on('timeout', function() {
-        req.abort();
-    });
-});
 
-/*
-req.setTimeout(3000, function(e){
-console.log("safasfsf" + e);
-req.abort();
-*/
 
-//}//);
+
+//	req.setTimeout(1000, function(e){
+//	console.log("timeout: " + e);
+
+//});
+
+req.end();
+
 
 } //close function
 
--- test.js
+++ test.js
@@ -2,7 +2,8 @@
 
 var fin = require("./finQuery");
 //NASDAQ%3AAAPL
-var appl = fin.query("","all",function(err,data){
+var appl = fin.query("NASDAQ%3AAAPL") // ,function(err,data){
+/*
 	if(err)
 	{
 		console.log("test.js callback error: " + err);
@@ -10,11 +11,12 @@ var appl = fin.query("","all",function(err,data){
 	else
 	{
 		console.log("\n---------------------------------------\ncallback in TEST.JS")
-				
+			
 		Object.keys(data).forEach(function(element, key, _array) {
 			console.log("" + element + "=" + data[element]);
 		});
 	}
 
 });
-
+*/
+console.log("the finish test.js file");
