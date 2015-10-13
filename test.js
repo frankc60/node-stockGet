ef9fc8670b53b35b147624faff4523344e911cd8
frankc60 <frankc60@gmail.com>
2015-10-13 14:32:13 +0100

    updated error handling different scenarios

--- test.js
+++ test.js
@@ -2,8 +2,8 @@
 
 var fin = require("./finQuery");
 //NASDAQ%3AAAPL
-var appl = fin.query("NASDAQ%3AAAPL") // ,function(err,data){
-/*
+console.log("call 1st fin");
+var appl = fin.query("NASDAQ%3AAAPL","all" ,function(err,data){
 	if(err)
 	{
 		console.log("test.js callback error: " + err);
@@ -18,5 +18,38 @@ var appl = fin.query("NASDAQ%3AAAPL") // ,function(err,data){
 	}
 
 });
-*/
+console.log("call 2nd fin");
+var appl = fin.query("NYSE%3AF","price" ,function(err,data){
+	if(err)
+	{
+		console.log("test.js callback error: " + err);
+	}
+	else
+	{
+		console.log("\n---------------------------------------\ncallback in TEST.JS")
+			
+		Object.keys(data).forEach(function(element, key, _array) {
+			console.log("" + element + "=" + data[element]);
+		});
+	}
+
+});
+console.log("call 3rd fin");
+var appl = fin.query("NxxAF","price" ,function(err,data){
+	if(err)
+	{
+		console.log("test.js callback error: " + err);
+	}
+	else
+	{
+		console.log("\n---------------------------------------\ncallback in TEST.JS")
+			
+		Object.keys(data).forEach(function(element, key, _array) {
+			console.log("" + element + "=" + data[element]);
+		});
+	}
+
+});
+
+
 console.log("the finish test.js file");
